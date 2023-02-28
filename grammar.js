f9d87727eade7f8fff4a546be29b02949f8025ab
Amaan Qureshi <amaanq12@gmail.com>
2023-02-27 23:13:56 -0500

    feat: complete the grammar, rename rules accordingly, add file doc

--- grammar.js
+++ grammar.js
@@ -1,12 +1,27 @@
+/**
+ * @file PO grammar for tree-sitter
+ * @author Erasin Wang <erasinoo@gmail.com>
+ * @author Amaan Qureshi <amaanq12@gmail.com>
+ * @license MIT
+ * @see {@link https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html|official syntax spec}
+ */
+
+/* eslint-disable arrow-parens */
+/* eslint-disable camelcase */
+/* eslint-disable-next-line spaced-comment */
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
 module.exports = grammar({
-  // https://www.gnu.org/software/gettext/manual/html_node/PO-Files.html
   name: 'po',
 
   rules: {
-    source_file: $ => repeat(choice(
-      $.message,
-      $.comment,
-    )),
+    source_file: $ => repeat(
+      choice(
+        $.message,
+        $.comment,
+      ),
+    ),
 
     message: $ => seq(
       optional($.msgctxt),
@@ -16,47 +31,101 @@ module.exports = grammar({
       optional($.msgstr_plural),
     ),
 
+    msgctxt: $ => seq('msgctxt', $.string),
+
     msgid: $ => seq(
-      choice("msgid ", seq("msgid[", $.number, "] ")),
-      repeat($.string)
+      choice(
+        'msgid',
+        seq('msgid', '[', $.number, ']'),
+      ),
+      repeat1($.string),
+    ),
+
+    msgid_plural: $ => seq(
+      choice(
+        'msgid_plural',
+        seq('msgid_plural', '[', $.number, ']'),
+      ),
+      repeat1($.string),
     ),
 
     msgstr: $ => choice(
-      repeat1(seq("msgstr[", $.number, "] ", repeat($.string))),
-      seq("msgstr ", repeat($.string)),
+      repeat1(seq(
+        'msgstr',
+        '[',
+        $.number,
+        ']',
+        repeat1($.string),
+      )),
+      seq('msgstr', repeat1($.string)),
     ),
 
-    msgctxt: $ => seq("msgctxt", $.string),
-    msgid_plural: $ => seq(
-      choice("msgid_plural ", seq("msgid_plural[", $.number, "] ")),
-      $.string
-    ),
     msgstr_plural: $ => seq(
-      choice("msgstr_plural ", seq("msgstr_plural[", $.number, "] ")),
-      $.string
+      choice(
+        'msgstr_plural',
+        seq('msgstr_plural', '[', $.number, ']'),
+      ),
+      $.string,
     ),
 
     comment: $ => choice(
-      $.comment_translator,
-      $.comment_extracted,
-      $.comment_reference,
-      $.comment_flag,
-      $.msg_previous_id,
-      $.msg_previous_str,
-      $.comment_text,
+      $.translator_comment,
+      $.extracted_comment,
+      $.reference,
+      $.flag,
+      $.previous_untranslated_string,
+      seq('#', $.text),
     ),
 
-    comment_translator: $ => seq('#.', optional($.text)),
-    comment_extracted: $ => seq('#|', optional($.text)),
-    comment_reference: $ => seq('#:', optional($.text)),
-    comment_flag: $ => seq('#,', optional($.text)),
-    msg_previous_id: $ => seq('#~ msgid ', optional($.string)),
-    msg_previous_str: $ => seq('#/~ msgstr ', optional($.string)),
-    comment_text: $ => seq("#", $.text),
+    translator_comment: $ => seq('#.', optional($.text)),
+    extracted_comment: $ => seq(
+      '#|',
+      optional(choice('msgid', 'msgid_plural')),
+      $.string,
+    ),
+    reference: $ => seq('#:', optional($.text)),
+    flag: $ => seq('#,', optional($.text)),
+    previous_untranslated_string: $ => seq(
+      choice('#~', '#~|'),
+      optional(choice(
+        'msgctxt',
+        'msgid',
+        'msgid_plural',
+        'msgstr',
+        seq('msgstr', '[', $.number, ']'),
+      )),
+      $.string,
+    ),
 
-    _whitespace: (_) => repeat1(/[\t\v ]/),
-    number: _$ => token(/([0-9]+|[0-9][0-9,]+[0-9])(\.[0-9]*)?/),
-    string: _$ => token(/"[^"]*"/),
-    text: _$ => token(/.*/),
-  }
+    number: _ => /([0-9]+|[0-9][0-9,]+[0-9])(\.[0-9]*)?/,
+
+    string: $ => choice(
+      seq(
+        '"',
+        repeat(choice(
+          $.string_fragment,
+          $._escape_sequence,
+        )),
+        '"',
+      ),
+    ),
+    string_fragment: _ => token.immediate(prec(1, /[^"\\]+/)),
+
+    _escape_sequence: $ =>
+      choice(
+        prec(2, token.immediate(seq('\\', /[^abfnrtvxu'\"\\\?]/))),
+        prec(1, $.escape_sequence),
+      ),
+    escape_sequence: _ => token.immediate(seq(
+      '\\',
+      choice(
+        /[^xu0-7]/,
+        /[0-7]{1,3}/,
+        /x[0-9a-fA-F]{2}/,
+        /u[0-9a-fA-F]{4}/,
+        /u{[0-9a-fA-F]+}/,
+      ))),
+
+    text: _ => /.*/,
+  },
 });
