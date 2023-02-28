3e3230071a7945942b7c09e6e4c6672a007f55d7
Amaan Qureshi <amaanq12@gmail.com>
2023-02-27 23:18:25 -0500

    feat: add Swift bindings

---
+++ bindings/swift/po.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_PO_H_
+#define TREE_SITTER_PO_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern TSLanguage *tree_sitter_po();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_PO_H_
